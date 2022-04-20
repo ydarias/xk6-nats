467881daee200fbe1ae6b1956265dacd88944173
Oleg Bespalov <oleg.bespalov@grafana.com>
2022-04-20 14:05:51 +0200

    feat: use k6's new module API

--- nats.go
+++ nats.go
@@ -1,49 +1,87 @@
 package nats
 
 import (
-	"context"
 	"crypto/tls"
 	"fmt"
 	"time"
 
+	"github.com/dop251/goja"
 	natsio "github.com/nats-io/nats.go"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
 )
 
 func init() {
-	modules.Register("k6/x/nats", new(Nats))
+	modules.Register("k6/x/nats", new(RootModule))
 }
 
+// RootModule is the global module object type. It is instantiated once per test
+// run and will be used to create k6/x/nats module instances for each VU.
+type RootModule struct{}
+
+// ModuleInstance represents an instance of the module for every VU.
 type Nats struct {
-	conn *natsio.Conn
+	conn    *natsio.Conn
+	vu      modules.VU
+	exports map[string]interface{}
 }
 
-// XNats JS constructor
-func (n *Nats) XNats(ctx *context.Context, configuration Configuration) (interface{}, error) {
-	rt := common.GetRuntime(*ctx)
+// Ensure the interfaces are implemented correctly.
+var (
+	_ modules.Instance = &Nats{}
+	_ modules.Module   = &RootModule{}
+)
+
+// NewModuleInstance implements the modules.Module interface and returns
+// a new instance for each VU.
+func (r *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	mi := &Nats{
+		vu:      vu,
+		exports: make(map[string]interface{}),
+	}
+
+	mi.exports["Nats"] = mi.client
+
+	return mi
+}
+
+// Exports implements the modules.Instance interface and returns the exports
+// of the JS module.
+func (mi *Nats) Exports() modules.Exports {
+	return modules.Exports{
+		Named: mi.exports,
+	}
+}
+
+func (n *Nats) client(c goja.ConstructorCall) *goja.Object {
+	rt := n.vu.Runtime()
+
+	var cfg Configuration
+	err := rt.ExportTo(c.Argument(0), &cfg)
+	if err != nil {
+		common.Throw(rt, fmt.Errorf("Nats constructor expect Configuration as it's argument: %w", err))
+	}
 
 	natsOptions := natsio.GetDefaultOptions()
-	natsOptions.Servers = configuration.Servers
-	if configuration.Unsafe {
+	natsOptions.Servers = cfg.Servers
+	if cfg.Unsafe {
 		natsOptions.TLSConfig = &tls.Config{
 			InsecureSkipVerify: true,
 		}
 	}
-	if configuration.Token != "" {
-		natsOptions.Token = configuration.Token
+	if cfg.Token != "" {
+		natsOptions.Token = cfg.Token
 	}
 
-	c, err := natsOptions.Connect()
+	conn, err := natsOptions.Connect()
 	if err != nil {
-		return nil, err
+		common.Throw(rt, err)
 	}
 
-	p := &Nats{
-		conn: c,
-	}
-
-	return common.Bind(rt, p, ctx), nil
+	return rt.ToValue(&Nats{
+		vu:   n.vu,
+		conn: conn,
+	}).ToObject(rt)
 }
 
 func (n *Nats) Close() {
